f1a933cad27b90316ef2e32abebd817a6fd7cea3
Luz Esperanza Méndez Reyes <99372132+LuzEsperanza-K020@users.noreply.github.com>
2022-04-06 22:24:48 -0500

    ControlGridView terminado

---
+++ ControlGridView/MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ControlGridView
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private ObservableCollection<Persona> personas = new ObservableCollection<Persona>();
+        private PersonaDataObject personaDao = new PersonaDataObject();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            personas = new ObservableCollection<Persona>(personaDao.listar());
+            ListViewAgenda.ItemsSource = personas;
+        }
+    }
+}
---
+++ ControlGridView/PersonaDataObject.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ControlGridView
+{
+   
+    internal class PersonaDataObject
+    {
+       private List<Persona> listaPersonas = new List<Persona>();
+
+        public PersonaDataObject()
+        {
+            Persona p1 = new Persona { Nombre = "Javier", Email = "javier@email.com", Genero = "hombre", Telefono = "2228889993", ImagenGenero = "C:\\muñeco.png" };
+            Persona p2 = new Persona { Nombre = "Rebeca", Email = "rebecca@email.com", Genero = "mujer", Telefono = "2226655447", ImagenGenero = "C:\\mujer.png" };
+            listaPersonas.Add(p1);
+            listaPersonas.Add(p2);
+        }
+
+        public List<Persona> listar()
+        {
+            return listaPersonas;
+        }
+    }
+}
